001aea19af1e86e49eca2d8eaec678dd8d2b9227
Rick Wu <rickwu316@gmail.com>
2021-01-05 19:20:44 +0800

    feat:init express&mongoose

---
+++ models/shortenURL.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const shortenURL = new Schema({
+    originalURL: {
+        type: String, // 資料型別是字串
+        required: true // 這是個必填欄位
+    },
+    shortenURL: {
+        type: String, // 資料型別是字串
+        required: true // 這是個必填欄位
+    }
+})
+module.exports = mongoose.model('shortenURL', shortenURLSchema)
