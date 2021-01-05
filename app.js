f749a2f52e8463a16ccef0d11a666e3523536f59
Rick Wu <rickwu316@gmail.com>
2021-01-06 03:22:54 +0800

    feat: create shorten function and will search the same web address

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
 const generateURL = require('./URLcreater')
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 
+
+const shortenURL = require('./models/shortenURL')
 
 
 
@@ -20,7 +23,7 @@ db.once('open', () => {
 })
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -30,6 +33,42 @@ app.get('/', (req, res) => {
     res.render('index')
     // console.log(generatePassword(5))
 })
+
+app.post('/', async (req, res) => {
+    const originalURL = req.body.originalURL
+    console.log(req.body)
+    let randomURL = generateURL(5)
+    console.log(URL)
+    // await shortenURL.find({ shortenURL: randomURL })
+    //     .then(URL => {
+    //         while (!URL === "") {
+    //             let randomURL = generateURL(5)
+    //             console.log(URL)
+    //         }
+
+    //     })
+    console.log(randomURL)
+    await shortenURL.find({ originalURL: originalURL })
+        .then(URL => {
+            console.log(URL)
+            if (URL === "") {
+                console.log('網頁已存在')
+                res.render('show', { URL })
+            } else {
+                return shortenURL.create({ originalURL: originalURL, shortenURL: randomURL })
+                    .then(() => {
+                        res.redirect('/show')
+                        console.log("成功")
+                    })
+            }
+        })
+
+})
+
+app.get('/show', (req, res) => {
+    res.render('show')
+})
+
 
 // 設定 port 3000
 app.listen(3000, () => {
